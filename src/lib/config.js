f82f255efc3f3d3e1072f7419451a93d6f155f3d
emilyhunt <emily.hunt.physics@gmail.com>
2025-09-05 15:24:36 +0200

    Update email address

--- src/lib/config.js
+++ src/lib/config.js
@@ -11,7 +11,7 @@ export const socials = {
 	mastodon: 'https://mstdn.social/@emilydoesastro',
 	twitter: 'https://twitter.com/emilydoesastro',
 	github: 'https://github.com/emilyhunt',
-	email: 'mailto:emily.hunt.physics@gmail.com'
+	email: 'mailto:emily.lauren.hunt@univie.ac.at'
 };
 
 /* BLOG SETTINGS */
